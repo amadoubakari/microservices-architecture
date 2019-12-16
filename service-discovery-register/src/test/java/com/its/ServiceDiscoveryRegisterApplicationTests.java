dd3d42594e2faee54334e3a7491acd2b61173383
AMADOU BAKARI <amadou_bakari@yahoo.fr>
2019-12-16 17:39:35 +0100

    Initialisation du message.

---
+++ service-discovery-register/src/test/java/com/its/ServiceDiscoveryRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.its;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
